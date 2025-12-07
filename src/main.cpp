a97f89837932db44a93a55d5629b3bc45c648089
sabiinsearch <sabiinsearch@gmail.com>
2025-12-07 14:24:41 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,32 @@
- #include <Arduino.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <AS5600.h>
 
+AMS_5600 ams5600;
+#define DIR_PIN 23
 
-
+int angle_in = 0;
 
 void setup() {
-  Serial.begin(9600);
-//  pinMode(potPin, INPUT);
-//  pinMode(DS18B20PIN, INPUT);
-//  pinMode(turbuditySensorPin, INPUT);
-
-  // pinMode (LED_BUILTIN, OUTPUT);
-
-  // wifi_init();
-  // qubitro_init();
-Serial.println(F("----------IoT Water Level Monitoring System using AS5600 magnetic sensor2----------"));
+  pinMode(DIR_PIN, OUTPUT);
+  Serial.begin(115200);
+  Wire.begin();
 }
+
+int Angle() {
+  digitalWrite(DIR_PIN, HIGH);
+  int in;
+  in = map(ams5600.getRawAngle(),0,4095,0,360);
+  return in;
+}
+
 void loop() {
-  
+  if (ams5600.detectMagnet() == 1 ) {
+    if (angle_in != Angle()) {
+      angle_in = Angle();
+      Serial.println(angle_in);
+    }
+  }
 }
 
 
