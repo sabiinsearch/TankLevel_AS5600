50177c893d773f82faf46ce53787e6f877980d9e
sabiinsearch <sabiinsearch@gmail.com>
2025-12-07 13:35:51 +0530

    init_publish

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+ #include <Arduino.h>
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+//  pinMode(potPin, INPUT);
+//  pinMode(DS18B20PIN, INPUT);
+//  pinMode(turbuditySensorPin, INPUT);
+
+  // pinMode (LED_BUILTIN, OUTPUT);
+
+  // wifi_init();
+  // qubitro_init();
+Serial.println(F("----------IoT Water Level Monitoring System using AS5600 magnetic sensor2----------"));
+}
+void loop() {
+  
+}
+
+
